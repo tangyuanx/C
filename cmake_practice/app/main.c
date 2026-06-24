4773fb0ae9237f473386a62622f5223df13b8758
xuesinian <tangyuan.sn@gmail.com>
2026-06-24 23:44:25 +0800

    add cmake practice

---
+++ cmake_practice/app/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "math_utils.h"
+
+int main(void)
+{
+    printf("3 + 5 = %d\n", add(3, 5));
+    printf("3 * 5 = %d\n", mul(3, 5));
+    return 0;
+}
---
+++ cmake_practice/math/src/math_utils.c
@@ -0,0 +1,11 @@
+#include "math_utils.h"
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int mul(int a, int b)
+{
+    return a * b;
+}
